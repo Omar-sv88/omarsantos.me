847244b9bfa338e6348bcfbd4a493b303f947824
Omar Santos Ventoso <contacto@omarsantos.me>
2022-04-19 21:03:52 +0200

    feat(Wrapper.vue): Add slot before container and withMarginBottom prop

--- components/Wrapper.vue
+++ components/Wrapper.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
     <Navigation />
+    <slot name="before-container"></slot>
     <section
-      class="container mx-auto my-8 px-4 lg:flex lg:gap-8 lg:justify-between"
+      class="container mx-auto px-4 lg:flex lg:gap-8 lg:justify-between"
+      :class="{ 'my-8': withMarginBottom }"
     >
       <slot name="content"></slot>
     </section>
@@ -16,6 +18,12 @@ import Navigation from './Navigation.vue'
 export default {
   components: {
     Navigation,
+  },
+  props: {
+    withMarginBottom: {
+      type: Boolean,
+      default: true,
+    },
   },
 }
 </script>
