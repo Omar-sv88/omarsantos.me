95a0ea22dd0e1e045a434d8b5c3e82b8414cbd2f
Omar Santos Ventoso <contacto@omarsantos.me>
2022-04-20 20:10:37 +0200

    fix(Wrapper.vue): Add margin-block in content section

--- components/Wrapper.vue
+++ components/Wrapper.vue
@@ -3,7 +3,7 @@
     <Navigation :change-bg-on-scroll="changeNavBgOnScroll" />
     <slot name="before-container"></slot>
     <section
-      class="container mx-auto px-8 lg:flex lg:gap-8 lg:justify-between"
+      class="container my-12 mx-auto px-8 lg:flex lg:gap-8 lg:justify-between"
       :class="{ 'pt-20': withPaddingTop }"
     >
       <slot name="content"></slot>
