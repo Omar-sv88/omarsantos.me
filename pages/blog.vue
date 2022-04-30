adf28428e4489943784110d6dba950e35ab3f803
Omar Santos Ventoso <contacto@omarsantos.me>
2022-04-30 18:31:39 +0200

    feat: Add blog view

---
+++ pages/blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <Wrapper>
+    <template #content-sidebar>
+      <ArticleList title="" />
+      <CategoryList />
+    </template>
+    <template #footer>
+      <Footer />
+    </template>
+  </Wrapper>
+</template>
+
+<script>
+import ArticleList from '~/components/shared/ArticleList.vue'
+import Wrapper from '~/components/layout/Wrapper.vue'
+import Footer from '~/components/layout/Footer.vue'
+import CategoryList from '~/components/shared/CategoryList.vue'
+
+export default {
+  components: { ArticleList, Wrapper, Footer, CategoryList },
+}
+</script>
