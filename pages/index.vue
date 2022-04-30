47135f6831b9741a5750853180cee43a92f66087
Omar Santos Ventoso <contacto@omarsantos.me>
2022-04-30 16:43:15 +0200

    Remove CategoryListComponent from index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,7 @@
     </template>
     <template #content>
       <AboutMe />
-    </template>
-    <template #content-sidebar>
-      <ArticleList class="my-12" />
-      <CategoryList class="my-12" />
+      <ArticleList :can-show-pagination="false" class="my-12" />
     </template>
     <template #footer>
       <Footer />
@@ -21,10 +18,9 @@ import Wrapper from '~/components/layout/Wrapper.vue'
 import Hero from '~/components/home/Hero.vue'
 import AboutMe from '~/components/home/AboutMe.vue'
 import ArticleList from '~/components/shared/ArticleList.vue'
-import CategoryList from '~/components/shared/CategoryList.vue'
 import Footer from '~/components/layout/Footer.vue'
 
 export default {
-  components: { ArticleList, Wrapper, Footer, CategoryList, Hero, AboutMe },
+  components: { ArticleList, Wrapper, Footer, Hero, AboutMe },
 }
 </script>
